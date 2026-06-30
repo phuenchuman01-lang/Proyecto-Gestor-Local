09e87578e82e219e4e3ce44803ab39e0b5c0fe4b
Luih <lgarias.cl@gmail.com>
2026-06-29 22:57:25 -0400

    Dev-Luis: crear estructura y metodo setUp para CasilleroControllerTest

---
+++ src/test/java/controlador/CasilleroControllerTest.java
@@ -0,0 +1,19 @@
+package controlador;
+
+import modelo.Casillero;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CasilleroControllerTest {
+
+    private CasilleroController controlador;
+
+    @BeforeEach
+    void setUp() {
+        controlador = new CasilleroController();
+    }
+
+
+
+}
