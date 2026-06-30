b973f068399484c462433a2dc437267df83cf012
Luih <lgarias.cl@gmail.com>
2026-06-30 00:09:51 -0400

    Dev-Luis: probar funcionamiento de registro y liberacion de casilleros.

--- src/test/java/controlador/CasilleroControllerTest.java
+++ src/test/java/controlador/CasilleroControllerTest.java
@@ -12,8 +12,32 @@ class CasilleroControllerTest {
     @BeforeEach
     void setUp() {
         controlador = new CasilleroController();
+
+        for (Casillero c : controlador.obtenerTodosLosCasilleros()) {
+            c.liberar();
+        }
+        persistencia.JsonManager.guardarCasilleros(controlador.obtenerTodosLosCasilleros());
     }
 
+    @Test
+    void testRegistrarCasilleroExitosamente() {
+        controlador.registrarCasillero(1, "alumnoTest");
 
+        Casillero c = controlador.obtenerTodosLosCasilleros().get(0);
 
+        assertFalse(c.estaDisponible(), "El casillero debería aparecer como ocupado");
+        assertEquals("alumnoTest", c.getUsernameDueño(), "El dueño del casillero debe coincidir con el registrado");
+    }
+
+    @Test
+    void testLiberarCasillero() {
+        controlador.registrarCasillero(2, "alumnoTest2");
+
+        controlador.liberarCasillero(2);
+
+        Casillero c = controlador.obtenerTodosLosCasilleros().get(1);
+
+        assertTrue(c.estaDisponible(), "El casillero debería estar disponible tras liberarse");
+        assertNull(c.getUsernameDueño(), "El nombre del dueño debería ser null tras liberarse");
+    }
 }
